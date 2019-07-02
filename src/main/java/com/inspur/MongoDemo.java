faa24f74f3bf8f1df24f68c4a59890bc9d269950
yangjianwei <2017585616@qq.com>
2019-07-03 00:20:36 +0800

    mongo3_action

---
+++ src/main/java/com/inspur/MongoDemo.java
@@ -0,0 +1,19 @@
+package com.inspur;
+
+/**
+ * User: YANG
+ * Date: 2019/7/3-0:18
+ * Description: No Description
+ *
+ * 本操作是针对mongodb 第3版本的
+ *
+ */
+public class MongoDemo {
+	public static void main(String[] args){
+
+
+
+
+
+	}
+}
